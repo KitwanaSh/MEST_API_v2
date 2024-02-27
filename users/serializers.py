85c301545ac7c204e85be7254790619bac07fe3a
KitwanaSh <rodriguekitweze@gmail.com>
2024-02-27 13:09:23 +0000

    Feat: Initial commit

---
+++ users/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+    """
+    Serializer the User Model
+    Args:
+        - serializer
+    """
+    id = serializers.IntegerField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    phone_number = serializers.CharField()
+    username = serializers.CharField()
+    email = serializers.CharField()
+
+
+class CohortSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    description = serializers.CharField()
+    year = serializers.IntegerField()
+    start_date = serializers.DateField()
+    end_date = serializers.DateField()
+    author = UserSerializer(many=True)
+
+class CohortMemberSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    member = UserSerializer(many=False)
+    is_active = serializers.BooleanField()
+    author = serializers.CharField()
