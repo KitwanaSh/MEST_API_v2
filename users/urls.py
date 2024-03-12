78d7ddcee069c3d8c50e0988cd95b15a0713e1e4
KitwanaSh <rodriguekitweze@gmail.com>
2024-03-12 09:58:14 +0000

    Feat Update user urls

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("signup/", signup),
     path("user_login/", user_login),
     path("forgot_password/", ForgotPasswordAPIView.as_view()),
+    path("reset_password/", ResetPasswordAPIView.as_view()),
+    path("user_profile/", UserProfileAPIView.as_view()),
+    path("change_password/", ChangePasswordAPIView.as_view()),
 ]
